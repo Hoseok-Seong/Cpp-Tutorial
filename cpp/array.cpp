b957848bb56089dfa7eaaf0bbba9fd3ed82abf07
Hoseok Seong <shsshs0125@naver.com>
2023-07-21 00:44:47 +0900

    배열

---
+++ cpp/array.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10];
+    int b[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int d[10] = {
+        0,
+    }; // 배열 값은 모두 0
+    float f[20];
+    char s[6] = "HELLO";
+
+    // int a[10][10];
+
+    // char c[6] = "hello";
+    // int i;
+    // for(i = 0; c[i] != NULL; i++){
+    //     printf("%c", c[i]);
+    // }
+
+    // char c[10];
+    // char s[10];
+    // scanf("%s", c);
+    // gets(s);
+
+    // 문자열 복사
+    // char str1[] = "hello", str2[11];
+    // strcpy(str2, str1);
+    // puts(str1);
+    // puts(str2);
+
+    // 문자열 연결 함수
+    // char dest[30] = "C ", source[30] = "programmer";
+    // strcat(dest, source);
+    // puts(dest);
+
+    // 문자열 비교
+    // char str1[] = "programmer", str2[] = "Programmer";
+    // printf("%d", strcmp(str1, str2));
+
+    // char strTemp;
+    // char string[] = "hello world";
+
+    // 문자 검색
+    // strTemp = strchr(string, 'a');
+    // printf("%s \n", strTemp);
+
+    // strTemp = strchr(string, 'c');
+    // printf("%s", strTemp);
+
+    // 문자열 길이
+    // strlen(const char* s)
+
+    return 0;
+}
